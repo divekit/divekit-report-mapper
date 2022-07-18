7982cd60a44c7eaef35dd95339e51f1694eac252
pschm <philipp.schmeier@gmail.com>
2022-07-18 11:37:41 +0200

    add surefire parsing error flag

--- src/main/mappers/SurefireReportMapper.ts
+++ src/main/mappers/SurefireReportMapper.ts
@@ -3,11 +3,17 @@ import parser from 'xml2json'
 import {SuiteCollection} from '../model/SuiteCollection'
 import {ReportMapper} from './interface/ReportMapper'
 import {STATUS, SUITE_TYPE} from '../const/CoreConstants'
-import {logger} from '../config/Logger'
+import {ensureArray, parseJsonFile} from '../util'
 
 export class SurefireReportMapper implements ReportMapper {
     suites: SuiteCollection = new SuiteCollection()
     input: string[] = []
+    private errorOccurred = false
+
+    // CDataTag --> XML Tag which escapes all characters in it
+    private static wrapWithCDataTag(text: string): string {
+        return '<![CDATA[' + text + ']]>'
+    }
 
     name(): string {
         return 'Surefire'
@@ -20,75 +26,67 @@ export class SurefireReportMapper implements ReportMapper {
     }
 
     getXml(): string {
+        if (this.errorOccurred) return ''
         return parser.toXml(JSON.stringify(this.suites))
     }
 
     mapToUnified(): void {
         this.input.forEach(file => {
-            let json
-            try {
-                json = JSON.parse(parser.toJson(file))
-            } catch (e) {
-                logger.error('Could NOT parse ' + file)
+            const json = parseJsonFile(file)
+            if (!json || !json.testsuite) {
+                this.errorOccurred = true
                 return
             }
-            // const json = JSON.parse(parser.toJson(file))
-            const testsuite = json.testsuite
 
-            if (json.testsuite) {
-                testsuite.type = SUITE_TYPE.JUNIT
-                delete testsuite.properties
-
-                const nameSplit = testsuite.name.split('.')
-                testsuite.name = nameSplit[nameSplit.length - 1]
-
-                if (!testsuite.testcase.length) {
-                    testsuite.testcase = [testsuite.testcase]
-                }
-
-                // tslint:disable-next-line:radix
-                testsuite.status = parseInt(testsuite.failures) || parseInt(testsuite.errors) > 0 ? STATUS.FAILED : STATUS.PASSED
-                testsuite.tests = undefined
-                testsuite.skipped = undefined
-                testsuite.errors = undefined
-                testsuite.time = undefined
-
-                testsuite.testcase.map((testcase: any) => {
-                    testcase.status = testcase.failure || testcase.error ? STATUS.FAILED : STATUS.PASSED
-                    testcase.hidden = testcase.name.toLowerCase().includes('hidden')
-                    testcase.classname = undefined
-                    testcase.time = undefined
-                })
-
-                testsuite.testcase.forEach((testcase: any) => {
-                    if (testcase.error) {
-                        if (!testcase.error.length) {
-                            testcase.error = [testcase.error]
-                        }
-
-                        testcase.error.forEach((error: any) => {
-                            error.message = encodeURI(error.message)
-                            error.$t = this.wrapWithCDataTag(error.$t)
-                        })
-                    }
-                    if (testcase.failure) {
-                        if (!testcase.failure.length) {
-                            testcase.failure = [testcase.failure]
-                        }
-
-                        testcase.failure.forEach((failure: any) => {
-                            failure.message = encodeURI(failure.message)
-                            failure.$t = this.wrapWithCDataTag(failure.$t)
-                        })
-                    }
-                    delete testcase['system-out'] // delete complete stack trace
-                })
-                this.suites.testsuite.push(testsuite)
-            }
+            const testsuite = this.updateTestsuite(json.testsuite)
+            this.suites.testsuite.push(testsuite)
         })
     }
 
-    wrapWithCDataTag(text: string): string {
-        return '<![CDATA[' + text + ']]>'
+    private updateTestsuite(testsuite: any): any {
+        testsuite.type = SUITE_TYPE.JUNIT
+        delete testsuite.properties
+
+        const nameSplit = testsuite.name.split('.')
+        testsuite.name = nameSplit[nameSplit.length - 1]
+        testsuite.testcase = ensureArray(testsuite.testcase)
+
+        // tslint:disable-next-line:radix
+        testsuite.status = parseInt(testsuite.failures) || parseInt(testsuite.errors) > 0 ? STATUS.FAILED : STATUS.PASSED
+        testsuite.tests = undefined
+        testsuite.skipped = undefined
+        testsuite.errors = undefined
+        testsuite.time = undefined
+
+        testsuite.testcase.map((testcase: any) => {
+            testcase.status = testcase.failure || testcase.error ? STATUS.FAILED : STATUS.PASSED
+            testcase.hidden = testcase.name.toLowerCase().includes('hidden')
+            testcase.classname = undefined
+            testcase.time = undefined
+        })
+
+        testsuite.testcase.forEach((testcase: any) => {
+            this.updateTestcase(testcase)
+        })
+
+        return testsuite
+    }
+
+    private updateTestcase(testcase: any) {
+        if (testcase.error) {
+            testcase.error = ensureArray(testcase.error)
+            testcase.error.forEach((error: any) => {
+                error.message = encodeURI(error.message)
+                error.$t = SurefireReportMapper.wrapWithCDataTag(error.$t)
+            })
+        }
+        else if (testcase.failure) {
+            testcase.failure = ensureArray(testcase.failure)
+            testcase.failure.forEach((failure: any) => {
+                failure.message = encodeURI(failure.message)
+                failure.$t = SurefireReportMapper.wrapWithCDataTag(failure.$t)
+            })
+        }
+        delete testcase['system-out'] // delete complete log output
     }
 }
--- src/main/util.ts
+++ src/main/util.ts
@@ -1,4 +1,22 @@
+import parser from 'xml2json'
+import {logger} from './config/Logger'
+
 export function ensureArray(object: any | any[]): any[] {
     if (!object.length) return [object]
     return object
+}
+
+/**
+ * Try to parse the content of a given file to json.
+ * @param path path to a json file
+ * @return parsed json or undefined if an exception is thrown while parsing
+ */
+export function parseJsonFile(path: string): any | undefined {
+    let json
+    try {
+        json = JSON.parse(parser.toJson(path))
+    } catch (e) {
+        logger.error('Could NOT parse ' + path)
+    }
+    return json
 }
